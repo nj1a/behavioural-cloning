ca35289c63e1bfff1aac08cc3179afd8aef3165b
njia95 <nathan.jia@mail.utoronto.ca>
2016-12-30 04:29:24 -0500

    updated model to architecture used in Nvidia paper

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.layers.convolutional import Convolution2D
 from keras.layers.normalization import BatchNormalization
-from keras.layers.pooling import MaxPooling2D
-from keras.layers.core import Activation, Flatten, Dense, Dropout
-from skimage.io import imread
+from keras.layers.convolutional import Convolution2D
+from keras.layers.core import Flatten, Dense, Dropout
+from scipy.misc import imread, imresize
 import numpy as np
 
 # load the data
@@ -18,51 +17,30 @@ paths_training, paths_validation, angles_training, angles_validation = train_tes
                                                                                         test_size=0.15, random_state=42)
 nb_training = paths_training.shape[0]
 nb_validation = paths_validation.shape[0]
-nb_epoch = 2
+nb_epoch = 5
 
 
-# build the model
 def a_model():
     model = Sequential()
-
-    model.add(Convolution2D(64, 11, 11, border_mode='valid', subsample=(4, 4), input_shape=(160, 320, 3)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-
-    model.add(Convolution2D(256, 5, 5, border_mode='valid'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
-
-    model.add(Convolution2D(384, 3, 3))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-
-    model.add(Convolution2D(384, 3, 3))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-
-    model.add(Convolution2D(256, 1, 1))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
+    model.add(BatchNormalization(input_shape=(66, 200, 3)))
+    model.add(Convolution2D(24, 5, 5, activation='relu', subsample=(2, 2)))
+    model.add(Convolution2D(36, 5, 5, activation='relu', subsample=(2, 2)))
+    model.add(Convolution2D(48, 5, 5, activation='relu', subsample=(2, 2)))
+    model.add(Convolution2D(64, 3, 3, activation='relu', subsample=(1, 1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu', subsample=(1, 1)))
     model.add(Flatten())
-    model.add(Dense(4096))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Dense(256))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-
-    model.add(Dense(1))
+    model.add(Dense(1164, activation='relu'))
+    # model.add(Dropout(0.25))
+    model.add(Dense(100, activation='relu'))
+    # model.add(Dropout(0.25))
+    model.add(Dense(50, activation='relu'))
+    # model.add(Dropout(0.25))
+    model.add(Dense(10, activation='relu'))
+    # model.add(Dropout(0.25))
+    model.add(Dense(1, activation='relu'))
 
     model.summary()
     model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-
     return model
 
 
@@ -70,11 +48,13 @@ def generator(ins, outs):
     while 1:
         for (image_path, angle) in zip(ins, outs):
             path = ('./' + image_path).replace(' ', '')
-            img = imread(path)[None, :, :, :]
+            img = imread(path)
+            # fit into input shape (66, 200, 3)
+            resized = imresize(img, (100, 200))
+            cropped = resized[None, 34:, :, :]
             ang = np.asarray([angle], np.float64)
-            yield img, ang
+            yield cropped, ang
 
-# train the model
 my_model = a_model()
 my_model.fit_generator(generator(paths_training, angles_training), nb_training, nb_epoch,
                        validation_data=generator(paths_validation, angles_validation), nb_val_samples=nb_validation)
