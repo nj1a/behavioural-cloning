4a8c675700675d8c838fbb149ef4032a2e8df772
njia95 <nathan.jia@mail.utoronto.ca>
2017-01-01 22:14:50 -0500

    updated architecture to get closer to the one from Nvidia; resized image in drive

--- drive.py
+++ drive.py
@@ -1,19 +1,14 @@
 import argparse
 import base64
-import json
 
 import numpy as np
 import socketio
-import eventlet
 import eventlet.wsgi
-import time
 from PIL import Image
-from PIL import ImageOps
-from flask import Flask, render_template
+from flask import Flask
 from io import BytesIO
-
+from scipy.misc import imresize
 from keras.models import model_from_json
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 
 # Fix error with Keras and TensorFlow
 import tensorflow as tf
@@ -25,6 +20,7 @@ app = Flask(__name__)
 model = None
 prev_image_array = None
 
+
 @sio.on('telemetry')
 def telemetry(sid, data):
     # The current steering angle of the car
@@ -34,14 +30,17 @@ def telemetry(sid, data):
     # The current speed of the car
     speed = data["speed"]
     # The current image from the center camera of the car
-    imgString = data["image"]
-    image = Image.open(BytesIO(base64.b64decode(imgString)))
+    img_string = data["image"]
+    image = Image.open(BytesIO(base64.b64decode(img_string)))
     image_array = np.asarray(image)
+    image_array = imresize(image_array, (100, 200))
+    image_array = image_array[34:, :, :]
     transformed_image_array = image_array[None, :, :, :]
+
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.2
+    throttle = 0.4
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
@@ -54,8 +53,8 @@ def connect(sid, environ):
 
 def send_control(steering_angle, throttle):
     sio.emit("steer", data={
-    'steering_angle': steering_angle.__str__(),
-    'throttle': throttle.__str__()
+        'steering_angle': steering_angle.__str__(),
+        'throttle': throttle.__str__()
     }, skip_sid=True)
 
 
--- model.py
+++ model.py
@@ -1,63 +1,73 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Convolution2D
-from keras.layers.core import Flatten, Dense, Dropout
-from scipy.misc import imread, imresize
 import numpy as np
+from sklearn.model_selection import train_test_split
+from scipy.misc import imread, imresize
 
-# load the data
-driving_log = pd.read_csv('./driving_log.csv')
-# image_paths = pd.concat([driving_log['center'], driving_log['left'], driving_log['right']])
-# angles = pd.concat([driving_log['steering'], driving_log['steering'] + 0.3, driving_log['steering'] - 0.3])
-image_paths = driving_log['center']
-angles = driving_log['steering']
-paths_training, paths_validation, angles_training, angles_validation = train_test_split(image_paths, angles,
-                                                                                        test_size=0.15, random_state=42)
-nb_training = paths_training.shape[0]
-nb_validation = paths_validation.shape[0]
-nb_epoch = 5
+from keras.models import Sequential
+from keras.layers import Lambda, Conv2D, ELU, Dense, Flatten
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+
+
+def process_image(image_path):
+    path = ('./' + image_path).replace(' ', '')
+    img = imread(path)
+    # fit into input shape (66, 200, 3)
+    resized = imresize(img, (100, 200))
+    cropped = resized[34:, :, :]
+    return cropped
 
 
 def a_model():
-    model = Sequential()
-    model.add(BatchNormalization(input_shape=(66, 200, 3)))
-    model.add(Convolution2D(24, 5, 5, activation='relu', subsample=(2, 2)))
-    model.add(Convolution2D(36, 5, 5, activation='relu', subsample=(2, 2)))
-    model.add(Convolution2D(48, 5, 5, activation='relu', subsample=(2, 2)))
-    model.add(Convolution2D(64, 3, 3, activation='relu', subsample=(1, 1)))
-    model.add(Convolution2D(64, 3, 3, activation='relu', subsample=(1, 1)))
-    model.add(Flatten())
-    model.add(Dense(1164, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(10, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='relu'))
-
+    model = Sequential([
+        Lambda(lambda x: x / 127.5 - 1., input_shape=(66, 200, 3)),
+        Conv2D(24, 5, 5, init='he_normal', subsample=(2, 2)),
+        ELU(),
+        Conv2D(36, 5, 5, init='he_normal', subsample=(2, 2)),
+        ELU(),
+        Conv2D(48, 5, 5, init='he_normal', subsample=(2, 2)),
+        ELU(),
+        Conv2D(64, 3, 3, init='he_normal', subsample=(1, 1)),
+        ELU(),
+        Conv2D(64, 3, 3, init='he_normal', subsample=(1, 1)),
+        Flatten(),
+        ELU(),
+        Dense(1164, activation='relu'),
+        ELU(),
+        Dense(100, activation='relu'),
+        ELU(),
+        Dense(50, activation='relu'),
+        ELU(),
+        Dense(10, activation='relu'),
+        ELU(),
+        Dense(1)
+    ])
     model.summary()
-    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=0.0001), loss='mse')
     return model
 
+# load the data
+driving_log = pd.read_csv('./driving_log.csv')
+image_paths = pd.concat([driving_log['center'], driving_log['left'], driving_log['right']])
+image_paths = np.array(image_paths, dtype=pd.Series)
+angles = pd.concat([driving_log['steering'], driving_log['steering'] - 0.1, driving_log['steering'] + 0.1])
+angles = np.array(angles, dtype=pd.Series)
+# image_paths = driving_log['center']
+# angles = driving_log['steering']
 
-def generator(ins, outs):
-    while 1:
-        for (image_path, angle) in zip(ins, outs):
-            path = ('./' + image_path).replace(' ', '')
-            img = imread(path)
-            # fit into input shape (66, 200, 3)
-            resized = imresize(img, (100, 200))
-            cropped = resized[None, 34:, :, :]
-            ang = np.asarray([angle], np.float64)
-            yield cropped, ang
+images = np.array([process_image(path) for path in image_paths])
+angles = np.array([np.asarray([angle], np.float64) for angle in angles])
+
+paths_training, paths_validation, angles_training, angles_validation = train_test_split(images, angles, test_size=0.25,
+                                                                                        random_state=4242)
+nb_training = paths_training.shape[0]
+nb_validation = paths_validation.shape[0]
+nb_epoch = 1
 
 my_model = a_model()
-my_model.fit_generator(generator(paths_training, angles_training), nb_training, nb_epoch,
-                       validation_data=generator(paths_validation, angles_validation), nb_val_samples=nb_validation)
+generator = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, channel_shift_range=0.1)
+my_model.fit_generator(generator.flow(paths_training, angles_training), nb_training, nb_epoch,
+                       validation_data=generator.flow(paths_validation, angles_validation), nb_val_samples=nb_validation)
 
 # save the model
 with open("model.json", "w") as json_file:
